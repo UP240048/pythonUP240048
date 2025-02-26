ad92e5263a39f686dfdaabcec41a48b852168ee1
UP240048 <UP240048@alumnos.upa.edu.mx>
2025-02-25 18:42:05 -0600

    25225_1842

---
+++ Day_01.py
@@ -0,0 +1,14 @@
+#Inciso 1
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+print(3**2)
+print(3%2)
+print(3//2)
+
+#Incisco 2
+print("David")
+print("Padilla")
+print("México")
+print("I´m enjoying 30 days of Python")
