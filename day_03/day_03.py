68886db392a399fb1fd51e46e9d77bf87ab14218
UP240048 <UP240048@alumnos.upa.edu.mx>
2025-02-27 01:03:30 -0600

    270225_0103

---
+++ day_03/day_03.py
@@ -0,0 +1,15 @@
+#Day 3
+age = 18
+height = 1.78
+complex_var = 2+3j
+
+base_triangle = int(input("Enter the base of a triangle: "))
+height_triangle = int(input("Enter the height of a triangle: "))
+area_triangle = (base_triangle * height_triangle)/2
+print("The area of the triangle is ", area_triangle)
+
+side_a = int(input("Enter side A: "))
+side_b = int(input("Enter side B: "))
+side_c = int(input("Enter side C: "))
+perimeter = side_a + side_b + side_c
+print("The perimeter of the triangle is ", perimeter)
